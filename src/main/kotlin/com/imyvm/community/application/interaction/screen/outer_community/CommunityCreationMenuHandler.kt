f6a8dd58ede942f8a46c40d5fed0e564706e6151
Doohaey Re <doohaey@gmail.com>
2026-01-06 22:47:13 +0800

    feat: add cautions to creation confirmation

--- src/main/kotlin/com/imyvm/community/application/interaction/screen/outer_community/CommunityCreationMenuHandler.kt
+++ src/main/kotlin/com/imyvm/community/application/interaction/screen/outer_community/CommunityCreationMenuHandler.kt
@@ -2,10 +2,12 @@ package com.imyvm.community.application.interaction.screen.outer_community
 
 import com.imyvm.community.application.interaction.common.onCreateCommunity
 import com.imyvm.community.application.interaction.screen.CommunityMenuOpener
+import com.imyvm.community.infra.CommunityConfig
 import com.imyvm.community.inter.screen.ConfirmMenu
 import com.imyvm.community.inter.screen.component.ConfirmTaskType
 import com.imyvm.community.inter.screen.outer_community.CommunityCreationMenu
 import com.imyvm.community.inter.screen.outer_community.CommunityCreationRenameMenuAnvil
+import com.imyvm.community.util.Translator
 import com.imyvm.iwg.domain.component.GeoShapeType
 import net.minecraft.server.network.ServerPlayerEntity
 
@@ -25,8 +27,8 @@ fun runSwitchCommunityShape(
     geoShapeType: GeoShapeType,
     isManor: Boolean,
     runBack: (ServerPlayerEntity) -> Unit
-){
-    val newType = when(geoShapeType){
+) {
+    val newType = when (geoShapeType) {
         GeoShapeType.CIRCLE -> GeoShapeType.RECTANGLE
         GeoShapeType.RECTANGLE -> GeoShapeType.POLYGON
         GeoShapeType.POLYGON -> GeoShapeType.CIRCLE
@@ -44,7 +46,7 @@ fun runSwitchCommunityType(
     geoShapeType: GeoShapeType,
     isManor: Boolean,
     runBack: (ServerPlayerEntity) -> Unit
-){
+) {
     CommunityMenuOpener.open(player) { syncId ->
         CommunityCreationMenu(syncId, communityName, geoShapeType, !isManor, player, runBack)
     }
@@ -55,29 +57,52 @@ fun runConfirmCommunityCreation(
     communityName: String,
     geoShapeType: GeoShapeType,
     isManor: Boolean
-){
+) {
+    val price = if (isManor) CommunityConfig.PRICE_MANOR else CommunityConfig.PRICE_REALM
+    val typeMarkerText = if (isManor) Translator.tr("community.type.manor") else Translator.tr("community.type.realm")
+    val typeMarkerStr = typeMarkerText?.string ?: "Unknown-Type"
+    val creationCautions = listOf(
+        Translator.tr(
+            "ui.confirm.create.caution.creation_cost",
+            price.toString(),
+            typeMarkerStr
+        )?.string ?: "Creating this community request will cost $price coins as $typeMarkerStr.",
+        Translator.tr(
+            "ui.confirm.create.caution.refund"
+        ) ?.string ?: "The creation cost will be refunded only when the creation is rejected.",
+        Translator.tr(
+            "ui.confirm.create.caution.region"
+        ) ?.string ?: "The region you selected will be occupied by the community, but you can only modify it after administration approval.",
+        Translator.tr(
+            "ui.confirm.create.caution.final_check"
+        )?.string ?: "Please make sure all the information is correct before confirming."
+    )
+
     CommunityMenuOpener.open(player) { syncId ->
         ConfirmMenu(
             syncId = syncId,
             playerExecutor = player,
             confirmTaskType = ConfirmTaskType.CREATE_COMMUNITY,
-            cautions = listOf(
-                ""
-            ),
-            runExecutor = { onCreateCommunity(
-                player,
-                if (isManor) "manor" else "realm",
-                communityName,
-                geoShapeType.toString()
-            ) },
-            runBack = { CommunityMenuOpener.open(player) { syncId ->
-                CommunityCreationMenu(
-                    syncId = syncId,
-                    currentName = communityName,
-                    currentShape = geoShapeType,
-                    isCurrentCommunityTypeManor = isManor,
-                    playerExecutor = player,
-                ) {} }
+            cautions = creationCautions,
+            runExecutor = {
+                onCreateCommunity(
+                    player,
+                    if (isManor) "manor" else "realm",
+                    communityName,
+                    geoShapeType.toString()
+                )
+            },
+            runBack =
+            {
+                CommunityMenuOpener.open(player) { syncId ->
+                    CommunityCreationMenu(
+                        syncId = syncId,
+                        currentName = communityName,
+                        currentShape = geoShapeType,
+                        isCurrentCommunityTypeManor = isManor,
+                        playerExecutor = player,
+                    ) {}
+                }
             },
             communityName = communityName,
             communityType = if (isManor) "manor" else "realm",
